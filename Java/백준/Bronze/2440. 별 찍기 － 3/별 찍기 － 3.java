2e2c63b1b9506e4393e2dc5bf205a064d49ceb93
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2025-07-22 15:24:25 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 156 ms, Memory: 13988 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// 입력
+		Scanner sc = new Scanner(System.in);
+		
+		// 정수 A, B
+		int num = sc.nextInt();
+		
+		for(int i=0;i<num;i++) {
+			for(int j=num; j>i ; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}
