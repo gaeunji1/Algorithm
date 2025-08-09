c5e433e50684ccca3526f1effbe95ebbbc1e8f5b
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2025-08-10 02:03:17 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 404 ms, Memory: 111620 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		sb.append((int)(Math.pow(2, n)-1)).append('\n');
+		
+		Hanoi(n,1,2,3);
+		System.out.println(sb);
+	}
+	
+	public static void Hanoi(int n, int start, int mid, int to) {
+		
+		if(n ==1) {
+			sb.append(start+ " " + to + "\n");
+			return;
+		}
+		
+		
+		Hanoi(n-1, start, to, mid);
+		sb.append(start+ " " + to + "\n");
+		Hanoi(n-1, mid, start, to);
+	}
+}
