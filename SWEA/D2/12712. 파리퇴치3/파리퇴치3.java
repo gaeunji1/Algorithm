125272b9bc7fa9a6936d71a60a865c31933fbd35
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2025-08-10 23:07:51 +0900

    [D2] Title: 파리퇴치3, Time: 150 ms, Memory: 28,288 KB -BaekjoonHub

--- SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
+++ SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
@@ -1,64 +1,70 @@
-
 import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
+		// 입력
 		Scanner sc = new Scanner(System.in);
 		int tc = sc.nextInt();
-		
-		for(int t=1; t<=tc;t++) {
-			int N = sc.nextInt(), M=sc.nextInt();
+		for(int t = 1; t<=tc;t++) {
+			
+			// N 전체 배열 M 세기
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			// 배열 만들기
 			int[][] arr = new int[N][N];
-			for(int i = 0; i<N;i++) {
-				for(int j = 0; j<N;j++) {
+			for(int i = 0; i<N ; i++) {
+				for(int j = 0; j<N ; j++) {
 					arr[i][j] = sc.nextInt();
 				}
 			}
 			
-			// 상하좌우
-			int[] dx = {-1,1,0,0};
-			int[] dy = {0,0,-1,1};
-			// 좌상 우상 우하 좌하
-			int[] dxr = {-1,-1,1,1};
-			int[] dyr = {-1,1,1,-1};
+			// 델타 좌우상하
+			int[] dx = {0,0,-1,1};
+			int[] dy = {-1,1,0,0};
 			
-			int maxFly = 0, fly = 0;
+			// 델타 좌상,우상,좌하,우하
+			int[] dxr = {-1, -1, 1, 1};
+			int[] dyr = {-1, 1, -1, 1};
 			
-			for(int i = 0; i<N;i++) {
-				for(int j = 0; j<N; j++) {
-					int flyPlus =arr[i][j];
-					for(int x=0;x<4;x++) {
-						for(int y = 1;y<M;y++) {
-							int nx = i+dx[x]*y;
-							int ny = j + dy[x]*y;
-							if(nx>= 0 && nx<N&&ny>= 0&&ny<N) {
-								flyPlus += arr[nx][ny];
-							}
-						}
-					}
-					int flyX =arr[i][j];
-					for(int x=0;x<4;x++) {
-						for(int y = 1;y<M;y++) {
-							int nx = i+dxr[x]*y;
-							int ny = j + dyr[x]*y;
-							if(nx>= 0 && nx<N&&ny>= 0&&ny<N) {
-								flyX += arr[nx][ny];
-							}
-						}
-					}
+			int maxCount =0;
+			
+			// 분사
+			for(int i = 0; i<N; i++) {
+				for(int j = 0; j<N ; j++) {
+					int xCount = arr[i][j];
+					int plusCount = arr[i][j];
 					
-					if(maxFly<flyPlus) {
-						maxFly = flyPlus;
+					for(int d = 0; d<4; d++) {
+						for(int m = 1; m < M ; m++) {
+							// +로 분사
+							int nx = i + dx[d] * m;
+							int ny = j + dy[d] * m;
+							
+							if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+								plusCount += arr[nx][ny];
+							}
+							
+							// x로 분사
+							int nxr = i + dxr[d] * m;
+							int nyr = j + dyr[d] * m;
+							
+							if (nxr >= 0 && nxr < N && nyr >= 0 && nyr < N) {
+								xCount += arr[nxr][nyr];
+							}
+							
+						}
 					}
-					if(maxFly < flyX) {
-						maxFly = flyX;
-					}
+					if(maxCount < plusCount) maxCount = plusCount;
+					if(maxCount < xCount) maxCount = xCount;
+					
+					
 				}
 			}
-			System.out.println("#" + t+" "+maxFly);
-		}
 			
+			
+			System.out.println("#" + t + " " + maxCount);
 		}
 		
-	
+	}
 }
