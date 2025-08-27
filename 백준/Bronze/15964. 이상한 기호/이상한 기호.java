717390494a68f3721e18c5397a811321df81c9f1
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2025-08-27 16:39:21 +0900

    [Bronze V] Title: 이상한 기호, Time: 96 ms, Memory: 12872 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-	public static int math(int a, int b) {
-		int result = (a+b) * (a-b);
-		return result;
-	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		long a = sc.nextInt();
+		long b = sc.nextInt();
 		
-		System.out.println(math(a,b));
+		long result = (a+b) * (a-b);
+		
+		System.out.println(result);
 	}
 }
