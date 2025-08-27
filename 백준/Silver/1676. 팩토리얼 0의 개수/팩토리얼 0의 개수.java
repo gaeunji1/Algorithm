4e91933422bdd7e4f6f98fb5a9dedf73286a7f81
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2025-08-27 17:03:22 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 100 ms, Memory: 12892 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        long zeros = 0;
+        for (long p = 5; p <= n; p *= 5) {
+            zeros += n / p;
+        }
+
+        System.out.println(zeros);
+        sc.close();
+    }
+}
