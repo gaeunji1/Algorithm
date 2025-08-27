c86cd1354f837bb1841c6823802cc422376bd16e
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2025-08-27 16:34:53 +0900

    [Bronze III] Title: 학점계산, Time: 92 ms, Memory: 12740 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		
+		if(n.equals("A+")) {
+			System.out.println("4.3");
+		} else if(n.equals("A0")) {
+			System.out.println("4.0");
+		}else if(n.equals("A-")) {
+			System.out.println("3.7");
+		}else if(n.equals("B+")) {
+			System.out.println("3.3");
+		} else if(n.equals("B0")) {
+			System.out.println("3.0");
+		}else if(n.equals("B-")) {
+			System.out.println("2.7");
+		}else if(n.equals("C+")) {
+			System.out.println("2.3");
+		} else if(n.equals("C0")) {
+			System.out.println("2.0");
+		}else if(n.equals("C-")) {
+			System.out.println("1.7");
+		}else if(n.equals("D+")) {
+			System.out.println("1.3");
+		} else if(n.equals("D0")) {
+			System.out.println("1.0");
+		}else if(n.equals("D-")) {
+			System.out.println("0.7");
+		}else {
+			System.out.println("0.0");
+		}
+		
+	}
+}
