5a6c3bda44aa9eeed465643c16b902d8d29beb8f
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2025-09-02 16:38:27 +0900

    [Bronze IV] Title: 영수증, Time: 92 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = sc.nextInt();
+		
+		for(int i = 0; i<9; i++) {
+			int price = sc.nextInt();
+			sum -= price;
+		}
+		System.out.println(sum);
+	}
+}
