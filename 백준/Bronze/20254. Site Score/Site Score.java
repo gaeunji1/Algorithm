b848588fe7b041d876a83bb045bc0a7dc32547da
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2025-10-14 23:32:42 +0900

    [Bronze V] Title: Site Score, Time: 92 ms, Memory: 12820 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long UR = sc.nextLong(), TR = sc.nextLong(), UO = sc.nextLong(), TO = sc.nextLong();
+        System.out.println(56*UR + 24*TR + 14*UO + 6*TO);
+    }
+}
