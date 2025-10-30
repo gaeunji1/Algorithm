50cc6d7246937f000d98d023160c1306a383aa2a
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2025-10-30 10:23:33 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 80 ms, Memory: 12752 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		int n = sc.nextInt();
+
+		for (int t = 1; t <= tc; t++) {
+			int l = sc.nextInt();
+			int u = sc.nextInt();
+		}
+		System.out.println("비와이");
+	}
+}
