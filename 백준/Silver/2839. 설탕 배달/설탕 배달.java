411ec2e836d0a4f08a525c13e9bc85b8067aedeb
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2026-01-20 23:00:36 +0900

    [Silver IV] Title: 설탕 배달, Time: 92 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 
-        int answer = -1;
-        int five = n / 5;
+		int answer = -1;
+		int five = n / 5;
 
-        for (int f = five; f >= 0; f--) {
-            int remain = n - 5 * f;
-            if (remain % 3 == 0) {
-                int three = remain / 3;
-                answer = f + three;
-                break; // 최소 개수 보장
-            }
-        }
+		for (int i = five; i >= 0; i--) {
+			int remain = n - 5 * i;
+			if (remain % 3 == 0) {
+				int three = remain / 3;
+				answer = i + three;
+				break;
+			}
+		}
 
-        System.out.println(answer);
-    }
+		System.out.println(answer);
+
+	}
 }
