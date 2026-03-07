be9b889112ddd26c22d4bb322a9355dfbe2fef48
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2026-03-08 00:46:36 +0900

    [Bronze V] Title: 채플이 너무해, Time: 88 ms, Memory: 12816 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(n>=6){
+            System.out.println("Success!");
+        } else{
+            System.out.println("Oh My God!");
+        }
+        
+    }
+}
