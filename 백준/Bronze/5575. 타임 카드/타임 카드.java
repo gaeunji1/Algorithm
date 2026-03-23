2e296e67e726992c8f00055d2287ae432c529ff4
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2026-03-23 19:21:09 +0900

    [Bronze IV] Title: 타임 카드, Time: 96 ms, Memory: 12860 KB -BaekjoonHub

---
+++ 백준/Bronze/5575. 타임 카드/타임 카드.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            int ah = sc.nextInt();
+            int am = sc.nextInt();
+            int as = sc.nextInt();
+            int bh = sc.nextInt();
+            int bm = sc.nextInt();
+            int bs = sc.nextInt();
+
+            int start = (ah * 3600) + (am * 60) + as;
+            int end = (bh * 3600) + (bm * 60) + bs;
+
+            int Time = end - start;
+            int h = Time / 3600;
+            int m = (Time % 3600) / 60;
+            int s = (Time % 3600) % 60;
+
+            System.out.println(h + " " + m + " " + s);
+        }
+        sc.close();
+    }
+}
