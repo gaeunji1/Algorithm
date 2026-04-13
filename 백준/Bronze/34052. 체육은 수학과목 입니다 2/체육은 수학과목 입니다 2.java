903b3ada62835176365e32c120c09d1ee973fd8a
Gaeun Kim <151455492+gaeunji1@users.noreply.github.com>
2026-04-13 23:56:47 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			int sum = 0;
+			
+			for(int i = 0; i < 4; i++) {
+				sum += Integer.parseInt(br.readLine());
+			}
+			
+			if(sum <= 1500) {
+				System.out.println("Yes");
+			} else {
+				System.out.println("No");
+			}
+	}
+
+}
